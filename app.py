7b673cbd4d1d7b0360be47da3a8a1585d481d3b7
waxx567 <waynem567@gmail.com>
2022-12-18 06:34:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def login():
         user = request.form["nm"]
         session["user"] = user
 
-        found_user = users.query.filter_by(name=user).alfirstl()
+        found_user = users.query.filter_by(name=user).first()
         if found_user:
             session["email"] = found_user.email
         else:
