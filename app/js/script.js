7816036fce2d934ea3e792b9922d4a87bed620b7
waxx567 <waynem567@gmail.com>
2023-01-05 14:23:04 +0200

    RESET to end of hamburger

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger')
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function(){
     console.log("open hamburger");
 
-    if(header.classList.contains('open')) {
+    if(header.classList.contains('open')) { // Close hamburger menu
         header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
-    else {
+    else { // Open hamburger menu
         header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 })
+ 
