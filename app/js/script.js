6251aa7b2c8f7005302e298532b2798c3dd76e58
waxx567 <waynem567@gmail.com>
2023-01-30 09:52:01 +0200

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('Testing');
+const hamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", function(){
+    console.log("open hamburger");
+
+    if(btnHamburger.classList.contains("extended")) {
+        btnHamburger.classList.add("extended");
+    }
+    else {
+        btnHamburger.classList.remove("extended");
+    }
+});
