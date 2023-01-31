0c0cec1080ac792232a4eae4998f0d89c13fd449
waxx567 <waynem567@gmail.com>
2023-01-31 10:41:15 +0200

    Lock scrolling

--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,28 @@
-const hamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
-const fadeElements = document.querySelectorAll('.has-fade');
+const hamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElements = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', function() {
-    console.log('open');
+btnHamburger.addEventListener("click", function() {
+    console.log("hamburger open");
 
     // Close hamburger menu
-    if(header.classList.contains('open')) {
-        header.classList.remove('open');
+    if(header.classList.contains("open")) {
+        body.classList.remove("no-scroll");
+        header.classList.remove("open");
         fadeElements.forEach(function(element) {
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
         });
     }
     // Open hamburger menu
     else { 
-        header.classList.add('open');
+        body.classList.add("no-scroll");
+        header.classList.add("open");
         fadeElements.forEach(function(element) {
-            element.classList.remove('fade-out');
-            element.classList.add('fade-in');
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
         });
     }
 });
