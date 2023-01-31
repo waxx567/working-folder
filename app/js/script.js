8db99c773267e7f7a5f88ddfdb443a8a0f59d7e1
waxx567 <waynem567@gmail.com>
2023-01-31 10:21:12 +0200

    Same

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,21 @@
 const hamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-const fadeElements = document.querySelectorAll('.fade');
+const fadeElements = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
     console.log('open');
 
-    if(header.classList.contains('open')) { // Close hamburger menu
+    // Close hamburger menu
+    if(header.classList.contains('open')) {
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElements.forEach(function(element)) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        };
     }
-    else { // Open hamburger menu
+    // Open hamburger menu
+    else { 
         header.classList.add('open');
         fadeElements.forEach(function(element) {
             element.classList.remove('fade-out');
